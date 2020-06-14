04f60b038ba2e99b27105a5d4a6bc55efd09b783
Gilberto L <gilbertoquintero91@gmail.com>
2020-06-14 14:21:21 -0700

    Add hero and  rating models

---
+++ GilbertoHeroKare/Models/Hero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GilbertoHeroKare.Models
+{
+    public class Hero
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
+        public string Rating { get; set; }
+        public string Picture { get; set; }
+        public string RatingHistorial { get; set; }
+
+
+    }
+}
---
+++ GilbertoHeroKare/Models/Rating.cs
@@ -0,0 +1,9 @@
+﻿namespace GilbertoHeroKare.Models
+{
+    public class Rating
+    {
+        public int RatingId { get; set; }
+        public string Reviewer { get; set; }
+        public int Value { get; set; }
+    }
+}
