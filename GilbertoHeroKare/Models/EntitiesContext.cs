f8512abd5cbe6c949dbb387be2821d825f88122b
Gilberto L <gilbertoquintero91@gmail.com>
2020-06-15 13:48:19 -0700

    Add hero model and dbcontext

---
+++ GilbertoHeroKare/Models/EntitiesContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace GilbertoHeroKare.Models
+{
+    public class EntitiesContext : DbContext
+    {
+        public EntitiesContext() : base("EntitiesContext")
+        {
+        }
+
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+    }
+}
--- GilbertoHeroKare/Models/Hero.cs
+++ GilbertoHeroKare/Models/Hero.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace GilbertoHeroKare.Models
 {
     public class Hero
     {
+        public int HeroId { get; set; }
         public string Name { get; set; }
         public string Age { get; set; }
         public string City { get; set; }
         public string Rating { get; set; }
         public string Picture { get; set; }
-        public string RatingHistorial { get; set; }
+        public List<Rating> RatingHistorial { get; set; }
 
 
     }
