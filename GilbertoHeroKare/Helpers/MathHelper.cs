839802aac08448a5a94a16a23ada32f9f44102f4
Gilberto L <gilbertoquintero91@gmail.com>
2020-06-16 01:55:44 -0700

    Add heelper to calculate median

---
+++ GilbertoHeroKare/Helpers/MathHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GilbertoHeroKare.Helpers
+{
+    public static class MathHelper
+    {
+        public static decimal GetMedian(int [] numbers)
+        {
+            // Create a copy of the input, and sort the copy
+            Array.Sort(numbers);
+
+            int count = numbers.Length;
+            if (count == 0)
+            {
+                return 0;
+            }
+            else if (count % 2 == 0)
+            {
+                // count is even, average two middle elements
+                int a = numbers[count / 2 - 1];
+                int b = numbers[count / 2];
+                return (a + b) / 2m;
+            }
+            else
+            {
+                // count is odd, return the middle element
+                return numbers[count / 2];
+            }
+        }
+
+
+    }
+}
